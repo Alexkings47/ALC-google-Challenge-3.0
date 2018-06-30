d82aa07bed5a94dc4e706a50c872a8928908d68c
Alexkings47 <35541396+Alexkings47@users.noreply.github.com>
2018-07-01 00:15:59 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,31 +1,33 @@
 //function that clears the prvious table on clicking submit.
 function removeTable(){
-	var table=$('tr');
+	let table=$('tr');
 		table.remove();}
 //to create the table according to the input height and width.
 function makeGrid(){
-removeTable();
-	var row=$('#inputHeight').val();
-	var col=$('#inputWeight').val();
-	for(var i =1;i<=row;i++){
+//call on removeTable() to clear the previous table.
+	removeTable();
+	let row=$('#inputHeight').val();
+	let col=$('#inputWeight').val();
+	for(let i =1;i<=row;i++){
 	$('table').append("<tr></tr>");
-	for(var j=1;j<=col;j++){
+	for(let j=1;j<=col;j++){
 	//tr:last,to append the td to the last tr.
-		$('tr:last').append("<td height='5px' width='5px'></td>");
-		
+	$('tr:last').append("<td height='5px' width='5px'></td>");	
 }
 }//to set colour values for the cells using event listeners.
+//if a cell has a style attribute,remove it else add one to it.
 $('td').click( function (event){
-	var paint=$('#colorPicker').val();
-
+	let paint=$('#colorPicker').val();
 	if($(this).attr('style')){
-		$(this).removeAttr('style');
-	}else{$(this).attr('style','background-color:'+paint);}
+	$(this).removeAttr('style');
+	}else{
+	$(this).attr('style','background-color:'+paint);
+	}
 });
 }
 
 //event listeners to create table on submit(class;create).
 $('.create').click(function (k)
 	{k.preventDefault();
-   makeGrid();
+   	makeGrid();
 });
